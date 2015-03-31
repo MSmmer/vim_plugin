d4591dc05f1682e41550c639323d12758268fe2f
MSummer <1984803530@qq.com>
2015-03-31 15:35:43 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "backing up current vim config"
+today = `date+%Y%m%d`
+for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc $HOME/.vimrc.bundles; do [ -e $i ] && [ ! -L $i ] && mv $i $i.$today; done
+for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc $HOME/.vimrc.bundles; do [ -L $i ] && unlink $i ; done
+
+
+echo "setting up symlinks"
+lnif $CURRENT_DIR/vimrc $HOME/.vimrc
+lnif "$CURRENT_DIR/" "$HOME/.vim"
+
+
+echo "install vundle"
+if [ ! -e $CURRENT_DIR/bundle/vundle ]; then
+    echo "Installing Vundle"
+    git clone https://github.com/gmarik/vundle.git $CURRENT_DIR/bundle/vundle
+else
+    echo "Upgrade Vundle"
+    cd "$HOME/.vim/bundle/vundle" && git pull origin master
+fi
+
+echo "Step4: update/install plugins using Vundle"
+system_shell=$SHELL
+export SHELL="/bin/sh"
+vim -u $HOME/.vimrc +BundleInstall! +BundleClean +qall
+export SHELL=$system_shell
+
+echo "Install Done!"
